fc4df64039239a72349ef4d03753a5819111e905
ckkk <kf524334@gmail.com>
2026-04-18 14:03:45 +0800

    refactor(database): 将数据库访问从JDBC切换到R2DBC响应式实现
    - 替换application.yaml中的datasource配置为r2dbc配置
    - 添加r2dbc连接池配置，支持最大连接数和空闲时间设置
    - 在pom.xml中移除阻塞式web和mybatis-plus依赖
    - 引入spring-boot-starter-webflux替代传统的web starter
    - 添加spring-boot-starter-data-r2dbc和postgresql-r2dbc驱动依赖
    - 更新Sa-Token依赖为reactor响应式版本
    - 修改R响应类，增加Mono响应式方法支持
    - 实现响应式的成功和错误处理方法

--- src/main/java/cc/flexapi/model/response/R.java
+++ src/main/java/cc/flexapi/model/response/R.java
@@ -3,6 +3,7 @@ package cc.flexapi.model.response;
 
 import lombok.Data;
 import org.springframework.http.HttpStatus;
+import reactor.core.publisher.Mono;
 
 /**
  *
@@ -20,43 +21,90 @@ public class R<T> {
 
     private T data;
 
-    public R() {
+    private R() {
     }
 
-    public R(int code, String msg, T data) {
+    private R(int code, String msg, T data) {
         this.code = code;
         this.msg = msg;
         this.data = data;
     }
 
-    public static <T> R<T> error(String msg) {
+    /**
+     * 响应式 响应成功
+     *
+     * @param momo 响应数据
+     * @param <T>  响应数据类型
+     * @return 响应式R
+     */
+    public static <T> Mono<R<T>> ok(Mono<T> momo) {
+        return momo.map(R::okSync);
+    }
+
+    /**
+     * 响应式 响应失败
+     *
+     * @param msg 错误信息
+     * @param <T> 响应数据类型
+     * @return 响应式R
+     */
+    public static <T> Mono<R<T>> error(String msg) {
+        return Mono.just(errorSync(msg));
+    }
+
+    /**
+     * 响应式 响应失败
+     *
+     * @param code 错误码
+     * @param msg  错误信息
+     * @param <T>  响应数据类型
+     * @return 响应式R
+     */
+    public static <T> Mono<R<T>> error(Integer code, String msg) {
+        return Mono.just(errorSync(code, msg));
+    }
+
+    /**
+     * 响应式 响应失败
+     *
+     * @param status 错误码
+     * @param msg    错误信息
+     * @param <T>    响应数据类型
+     * @return 响应式R
+     */
+    private static <T> Mono<R<T>> error(HttpStatus status, String msg) {
+        return Mono.just(errorSync(status, msg));
+    }
+
+    private static <T> R<T> errorSync(String msg) {
         R<T> r = new R<>();
         r.setCode(500);
         r.setMsg(msg);
         return r;
     }
 
-    public static <T> R<T> error(HttpStatus status, String msg) {
+    private static <T> R<T> errorSync(HttpStatus status, String msg) {
         R<T> r = new R<>();
         r.setCode(status.value());
         r.setMsg(msg);
         return r;
     }
-    public static <T> R<T> error(Integer code, String msg) {
+
+    private static <T> R<T> errorSync(Integer code, String msg) {
         R<T> r = new R<>();
         r.setCode(code);
         r.setMsg(msg);
         return r;
     }
 
-    public static <T> R<T> ok() {
+    private static <T> R<T> okSync() {
         R<T> r = new R<>();
         r.setCode(200);
         r.setMsg("操作成功");
         return r;
     }
 
-    public static <T> R<T> ok(T data) {
+    private static <T> R<T> okSync(T data) {
         R<T> r = new R<>();
         r.setCode(200);
         r.setMsg("操作成功");
